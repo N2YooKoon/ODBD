72b755fd34d2773577ce50752e6bb92a9a00a1e6
Administrator <Administrator@User -2023HNAWH>
2023-08-18 10:12:24 +0900

    오두방뎅 웹페이지 본 프로젝트입니다. 모든 추가 및 수정 삭제는 협의를 통해 진행하고 각자의 브랜치를 이용하여 작업하십시오.

---
+++ odbd/src/main/java/com/odbd/OdbdApplication.java
@@ -0,0 +1,13 @@
+package com.odbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdbdApplication.class, args);
+	}
+
+}
---
+++ odbd/src/test/java/com/odbd/OdbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
